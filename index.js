fd8467789f441f030c6d7ce5f81a89dc20a9141a
Amy Jackson <aj81623@gmail.com>
2017-11-10 00:45:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 
   return driver;
 }
+function deleteFromDriverByKey(driver, key, value){
+  const newObj = Object.assign({}, driver);
+}
